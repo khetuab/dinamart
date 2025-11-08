2bf12d4532f5a4fe77db731f94aec73617fff79f
khetuab <khattababdu80@gmail.com>
2025-11-07 22:10:37 -0800

    first commit

--- server/server.js
+++ server/server.js
@@ -7,7 +7,10 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ["https://dinamart.netlify.app", "http://localhost:3000"], // your Netlify frontend
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
